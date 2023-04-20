394939317a082a8e8a93ecebfee3821db6a8ab18
Svitlana Morkva <lanamorkva.work@gmail.com>
2023-04-20 22:01:37 +0200

    Initial commit: linked libpng; added reading from and writing to png files

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include "pngReader.h"
+
+// random block generation
+void generateBlocks() {
+
+}
+
+void minCut() {
+
+}
+
+int getMinCutBlock() {
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    file::read_png_file("../gallery/input0.png");
+    file::write_png_file("../gallery/output0.png");
+    return 0;
+}
---
+++ pngReader.h
@@ -0,0 +1,9 @@
+#ifndef TEAM19_PNGREADER_H
+#define TEAM19_PNGREADER_H
+
+namespace file {
+    void read_png_file(char const *filename);
+    void write_png_file(char const *filename);
+}
+
+#endif //TEAM19_PNGREADER_H
