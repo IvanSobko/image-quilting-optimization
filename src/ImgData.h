e73523c6830d6dcce0340cb15e69c8be9722f1c9
Ivan Sobko <sobko.ivan@gmail.com>
2023-04-21 18:18:18 +0200

    fix for Windows build add gitignore and clang-format files

--- src/ImgData.h
+++ src/ImgData.h
@@ -1,13 +1,14 @@
 #ifndef TEAM19_IMGDATA_H
 #define TEAM19_IMGDATA_H
 
+#include <cstdint>
 #include <cstdlib>
 
 struct ImgData {
     // each char * stores one row of an image, for each pixel 4 value {rgba}
     unsigned char **data = NULL;
     unsigned char **output_d = NULL;
-    uint32_t width, height;
+    uint32_t width = 0, height = 0;
     uint32_t output_w = 0, output_h = 0;
     uint32_t block_w = 0, block_h = 0;
 };
