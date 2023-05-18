16b6c87c2cdb9879081c0ac3c25ec093bedb050b
Tal Rastopchin <rastopch@grinnell.edu>
2023-05-18 16:47:49 +0200

    implement allocating and randomizing the input data

--- src/ImgData.h
+++ src/ImgData.h
@@ -14,6 +14,14 @@ struct ImgData {
     uint32_t output_w = 0, output_h = 0;
     uint32_t block_w = 0, block_h = 0;
 
+    // Allocate memory for the input image
+    void AllocateInput() {
+        data = (unsigned char **) malloc(sizeof(unsigned char *) * height);
+        for (int y = 0; y < height; y++) {
+            data[y] = (unsigned char *) malloc(width * CHANNEL_NUM);
+        }
+    }
+
     // Free memory for the input texture
     void FreeInput(){
         for (int y = 0; y < height; y++) {
@@ -21,6 +29,24 @@ struct ImgData {
         }
         free(data);
         data = NULL;
+    }
+
+    // Generate a random number in the range [min, max]
+    int GetRandomInt(int min, int max) {
+        // https://stackoverflow.com/questions/1190870/i-need-to-generate-random-numbers-in-c
+        return rand() % (max - min + 1) + min;
+    }
+
+    // Randomize the input image
+    void RandomizeInput() {
+        const unsigned char unsigned_char_max = std::numeric_limits<unsigned char>::max();
+        for (int i = 0; i < height; i++){
+            for (int j = 0; j < width; j++){
+                for (int k = 0; k < CHANNEL_NUM - 1; k++)
+                    data[i][CHANNEL_NUM * (j) + k] = GetRandomInt(0, unsigned_char_max);
+                data[i][CHANNEL_NUM * (j) + 3] = unsigned_char_max;
+            }
+        }
     }
 
     // Allocate memory for the output texture and initialize it to black
@@ -37,24 +63,6 @@ struct ImgData {
                 for (int k = 0; k < CHANNEL_NUM; k++){
                     output_d[i][CHANNEL_NUM * (j) + k] = 0;
                 }
-            }
-        }
-    }
-
-    // Generate a random number in the range [min, max]
-    int GetRandomInt(int min, int max) {
-        // https://stackoverflow.com/questions/1190870/i-need-to-generate-random-numbers-in-c
-        return rand() % (max - min + 1) + min;
-    }
-
-    // Randomize the output image
-    void RandomizeOutput() {
-        const unsigned char unsigned_char_max = std::numeric_limits<unsigned char>::max();
-        for (int i = 0; i < output_h; i++){
-            for (int j = 0; j < output_w; j++){
-                for (int k = 0; k < CHANNEL_NUM - 1; k++)
-                    output_d[i][CHANNEL_NUM * (j) + k] = GetRandomInt(0, unsigned_char_max);
-                output_d[i][CHANNEL_NUM * (j) + 3] = unsigned_char_max;
             }
         }
     }
